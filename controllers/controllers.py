41af010c5a407449ea5abada513fbce35b3dd186
DiegoLopezTrastoy <sondiegolt@gmail.com>
2025-03-20 11:04:48 +0200

    fix?

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -9,11 +9,11 @@ class MyModule(http.Controller):
     @http.route(['/my/mis_certificados'], type='http', auth="user", website=True)
     def my_portalCertificatesListView(self, **kw):
         certificates = request.env['certificates'].search([('x_client', '=', request.env.user.partner_id.id)])
-        return request.render('my_portal.my_portal_certificates', {'certificates': certificates})
+        return request.render('my_portal.certificates', {'certificates': certificates})
 
     @http.route(['/my/request_certificate'], type='http', auth="user", website=True)
     def my_portalCertificatesForm(self, **kw):
-        return request.render('my_portal.my_portal_request_certificate')
+        return request.render('my_portal.request_certificate')
 
     @http.route(['/my/certificates/create'], type='http', auth="user", methods=['POST'], website=True)
     def create_certificate(self, **post):
@@ -27,12 +27,12 @@ class MyModule(http.Controller):
     @http.route(['/my/check_certificate'], type='http', auth="public", website=True)
     def my_portalCheckCertificates(self, **kw):
         certificate = request.env['certificates'].search([('x_uuid', '=', kw.get('UUID')), ('x_status', '=', 'Activo')])
-        return request.render('my_portal.my_portal_check_certificates', {'certificate': certificate})
+        return request.render('my_portal.check_certificates', {'certificate': certificate})
 
     @http.route(['/my/certificate'], type='http', auth="public", website=True)
     def my_portalCertificate(self, **kw):
         certificate = request.env['certificates'].search([('x_uuid', '=', kw.get('UUID')), ('x_status', '=', 'Activo')])
-        return request.render('my_portal.my_portal_see_certificate', {'certificate': certificate})
+        return request.render('my_portal.see_certificate', {'certificate': certificate})
 
     @http.route(['/my/printCertificate'], type='http', auth="user", website=True)
     def pruebas(self, **kw):
